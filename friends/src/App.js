1997b051d6ad347ecf70a18cd623ce21ef21e908
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-28 23:27:50 -0500

    [Add] Created login form and handleChanges method

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Login from './Components/Auth/Login';
+
 function App() {
   return (
     <div className="App">
-     
+        <Login />
     </div>
   );
 }
--- friends/src/Components/Auth/Login.js
+++ friends/src/Components/Auth/Login.js
@@ -4,15 +4,37 @@ class Login extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-
+            credentials: {
+                username: '',
+                password: ''
+            }
         }
+    }
+
+    handleChanges =  e => {
+   
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
     }
 
     render(){
         return(
-            <div className="login-container">
-
-            </div>
+           <div className="login-form-container">
+               <form action="" className="login-form">
+                   <div className="input-username-container">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" value={this.state.credentials.username} onChange={this.handleChanges}/>
+                   </div>
+                   <div className="input-password-container">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" value={this.state.credentials.password} onChange={this.handleChanges}/>
+                   </div>
+               </form>
+           </div>
         )
     }
 }
